6994733f9ca88652bc9244d8cd6357508dc7ada2
GB-Titi <tristanmatteoli@gmail.com>
2021-03-24 17:37:08 +0100

    intégration images header + carre sur borne, modifications graphiques

--- js/fonctions.js
+++ js/fonctions.js
@@ -5,27 +5,22 @@ function changeColor(color) {
   switch (color) {
     case "bleu":
       console.log(color);
-      borne.style.backgroundColor = "#0000ff";
       borne_arcade.style.backgroundImage = "url('../img/Bleu.png')";
       break;
     case "jaune":
-      borne.style.backgroundColor = "#ffff00";
       borne_arcade.style.backgroundImage = "url('../img/Jaune.png')";
       console.log(color);
       break;
     case "rouge":
-      borne.style.backgroundColor = "#ff0000"
       borne_arcade.style.backgroundImage = "url('../img/Rouge.png')";
       console.log(color);
       break;
     case "vert":
-      borne.style.backgroundColor = "#00ff00"
       borne_arcade.style.backgroundImage = "url('../img/Vert.png')";
       console.log(color);
       break;
 
     default:
-        borne.backgroudColor = black
 
       break;
   }
@@ -33,19 +28,16 @@ function changeColor(color) {
 
 var loadHeader = function(event) {
     var output = document.getElementById('img_header');
+    var borne_header = document.getElementById('borne_header');
     output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      // URL.revokeObjectURL(output.src) // free memory
-    }
-    //console.log(output.src)
+    borne_header.src = URL.createObjectURL(event.target.files[0]);
   };
 
 var loadCarre = function(event) {
     var output = document.getElementById('img_carre');
+    var borne_carre = document.getElementById('borne_carre');
     output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      // URL.revokeObjectURL(output.src) // free memory
-    }
-   // console.log(output.src)
+    borne_carre.src = URL.createObjectURL(event.target.files[0]);
+
   };
 
